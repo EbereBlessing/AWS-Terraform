2ced5acc3f22df1aa77dca2a4181e6440f598057
ebere.okey <okeyebereblessing@gmail.com>
2023-11-03 16:25:49 +0100

    EKS-setup

--- Terraform_EKS/EKS-main.tf
+++ Terraform_EKS/EKS-main.tf
@@ -1,15 +1,14 @@
-# main.tf
-
-## Terraform EKS Module
+## setting up EKS using Terraform modules
+## Modify to suit your specific needs
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.3"
 
-  cluster_name    = var.stack_name
+  cluster_name    = var.cluster_name
   cluster_version = "1.27"
 
-  vpc_id                         = aws_vpc.main.id 
-  subnet_ids                     = aws_subnet.private_subnet_01
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
@@ -40,6 +39,7 @@ module "eks" {
   }
 }
 
+
 # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
@@ -50,27 +50,19 @@ module "irsa-ebs-csi" {
   version = "4.7.0"
 
   create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${var.stack_name}"
+  role_name                     = "AmazonEKSTFEBSCSIRole-${var.cluster_name}"
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
 
 resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = var.stack_name
+  cluster_name             = var.cluster_name
   addon_name               = "aws-ebs-csi-driver"
   addon_version            = "v1.20.0-eksbuild.1"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-}
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
-}
-output "region" {
-  description = "AWS region"
-  value       = var.region
-}
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = var.stack_name
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
+  }
 }
--- Terraform_EKS/VPC-main.tf
+++ Terraform_EKS/VPC-main.tf
@@ -1,214 +1,47 @@
-# main.tf
-## Defining terraform provider
+## setting up VPC based on EKS VPC Specifications using Terraform modules
+## Modify to suit your specific needs
 provider "aws" {
   region = var.region
 }
-# Create VPC
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_block
-  enable_dns_support = true
+
+# Filter out local zones, which are not currently supported  with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+locals {
+  cluster_name = "eks-setup-${random_string.suffix.result}"
+}
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = var.vpc
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
 
-  tags = {
-    Name = "${var.stack_name}-VPC"
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
   }
 }
-# Create Internet Gateway
-resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id 
-    tags = {
-    "Name"        = "${var.stack_name}-igw"
-  }
-}
-
-# Create Public Route Table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name     = "Public Subnets Route Table"
-    Network  = "Public"
-  }
-}
-# Create Private Route Tables
-resource "aws_route_table" "private_01" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name     = "Private Route Table AZ1"
-    Network  = "Private01"
-  }
-}
-
-resource "aws_route_table" "private_02" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name     = "Private Route Table AZ2"
-    Network  = "Private02"
-  }
-}
-
-# Create Public Route
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main.id
-
-}
-
-# Create NAT Gateways
-resource "aws_eip" "nat_gateway_01" {
-     vpc        = true
-}
-
-resource "aws_eip" "nat_gateway_02" {
-     vpc        = true
-}
-
-resource "aws_nat_gateway" "nat_gateway_01" {
-  allocation_id = aws_eip.nat_gateway_01.id
-  count = 1  # This should match the count from the subnet block
-  subnet_id = aws_subnet.public_subnet_01[count.index].id
-
-
-  tags = {
-    Name = "${var.stack_name}-NatGatewayAZ1"
-  } 
-}
-
-resource "aws_nat_gateway" "nat_gateway_02" {
-  allocation_id = aws_eip.nat_gateway_02.id
-  count = 1
-  subnet_id     = aws_subnet.public_subnet_02[count.index].id
- 
-
-  tags = {
-    Name = "${var.stack_name}-NatGatewayAZ2"
-  }
-}
-
-# Create Public Subnets
-resource "aws_subnet" "public_subnet_01" {
-  count = 1
-  map_public_ip_on_launch = true
-  availability_zone = element(data.aws_availability_zones.available.names, 0)
-  cidr_block = var.public_subnet_01_block
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.stack_name}-PublicSubnet01"
-    "kubernetes.io/role/elb" = "1"
-  }
-}
-
-resource "aws_subnet" "public_subnet_02" {
-  count = 1
-  map_public_ip_on_launch = true
-  availability_zone = element(data.aws_availability_zones.available.names, 1)
-  cidr_block = var.public_subnet_02_block
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.stack_name}-PublicSubnet02"
-    "kubernetes.io/role/elb" = "1"
-  }
-}
-
-# Create Private Subnets
-resource "aws_subnet" "private_subnet_01" {
-  count = 1
-  availability_zone = element(data.aws_availability_zones.available.names, 0)
-  cidr_block = var.private_subnet_01_block
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.stack_name}-PrivateSubnet01"
-    "kubernetes.io/role/internal-elb" = "1"
-  }
-}
-
-resource "aws_subnet" "private_subnet_02" {
-  count = 1
-  availability_zone = element(data.aws_availability_zones.available.names, 1)
-  cidr_block = var.private_subnet_02_block
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.stack_name}-PrivateSubnet02"
-    "kubernetes.io/role/internal-elb" = "1"
-  }
-}
-
-# Associate Subnets with Route Tables
-resource "aws_route_table_association" "public_subnet_01" {
-  subnet_id      = aws_subnet.public_subnet_01[0].id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_subnet_02" {
-  subnet_id      = aws_subnet.public_subnet_02[0].id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "private_subnet_01" {
-  subnet_id      = aws_subnet.private_subnet_01[0].id
-  route_table_id = aws_route_table.private_01.id
-}
-
-resource "aws_route_table_association" "private_subnet_02" {
-  subnet_id      = aws_subnet.private_subnet_02[0].id
-  route_table_id = aws_route_table.private_02.id
-}
-
-# Create Security Group for Control Plane
-resource "aws_security_group" "control_plane" {
-  name_prefix = "Cluster communication with worker nodes"
-  vpc_id      = aws_vpc.main.id
-#   ingress {
-#    description = "Allow all traffic through port 8080"
-#    from_port = 8080
-#    to_port = 8080
-#    protocol = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-  # Since we only want to be able to SSH into the  EC2 instance, we are only
-  # allowing traffic from our IP on port 22
-  ingress {
-    description = "Allow SSH from my computer"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  # We want the  instance to being able to talk to the internet
-  egress {
-    description = "Allow all outbound traffic"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Define Outputs
-output "subnet_ids" {
-  description = "Subnets IDs in the VPC"
-  value = join(",", [aws_subnet.public_subnet_01[0].id, aws_subnet.public_subnet_02[0].id, aws_subnet.private_subnet_01[0].id, aws_subnet.private_subnet_02[0].id])
-}
-
-output "security_groups" {
-  description = "Security group for the cluster control plane communication with worker nodes"
-  value = aws_security_group.control_plane.id
-}
-
-output "vpc_id" {
-  description = "The VPC Id"
-  value = aws_vpc.main.id
-}
-
-# Data source to fetch available availability zones
-data "aws_availability_zones" "available" {}
-
-# Provide values for variables
-locals {
-  stack_name = "YourStackName" # Replace with your stack name
-}
-
