dd8e1d88d38de1ddc3eeb23c08fe726efe4b606b
ebere.okey <okeyebereblessing@gmail.com>
2023-11-17 17:30:11 +0100

    ALB-ASg

--- AWS-ALB-ASG/EC2.tf
+++ AWS-ALB-ASG/EC2.tf
@@ -3,19 +3,19 @@ resource "aws_key_pair" "keypair" {
   public_key = file(var.key_path)
 }
 # EC2 instance in Public Subnet
-#resource "aws_instance" "Bastion" {
-  #ami             = var.os # Replace with the desired AMI ID
-  #allocation_id = aws_eip.NAT_eip.id
-  #instance_type = var.instance   # Replace with the desired instance type
-  #vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  #subnet_id     = aws_subnet.public_subnet1.id
-  #associate_public_ip_address = true
-  #key_name      = aws_key_pair.keypair.key_name # Replace with your SSH key pair
- # tags = {
- #   Name = "Bastion"
- #   description = "Jump server to the private instance "
-|#  }
-#}
+#resource "aws_instance" "public_ec2" {
+  ami             = var.os # Replace with the desired AMI ID
+  allocation_id = aws_eip.NAT_eip.id
+  instance_type = var.instance   # Replace with the desired instance type
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  subnet_id     = aws_subnet.public_subnet1.id
+  associate_public_ip_address = true
+  key_name      = aws_key_pair.keypair.key_name # Replace with your SSH key pair
+  tags = {
+    Name = "ALB instances"
+    description = "EC2 instance "
+  }
+}
 resource "aws_launch_configuration" "ec2" {
   name                        = "${var.tag}-instances"
   image_id            = var.os  # Replace with the desired AMI ID
