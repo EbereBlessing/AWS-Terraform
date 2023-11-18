8616e9116515990e8f09e92a81b426f2d87494f9
Ebere Blessing <147432108+EbereBlessing@users.noreply.github.com>
2023-11-18 01:46:13 +0100

    Update EC2.tf

--- AWS-ALB-ASG/EC2.tf
+++ AWS-ALB-ASG/EC2.tf
@@ -1,7 +1,3 @@
-resource "aws_key_pair" "keypair" {
-  key_name   = "setup"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
 # EC2 instance in Public Subnet
  resource "aws_instance" "public_ec2" {
   ami             = var.os # Replace with the desired AMI ID
@@ -9,7 +5,7 @@ resource "aws_key_pair" "keypair" {
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   subnet_id     = aws_subnet.public_subnet1.id
   associate_public_ip_address = true
-  key_name      = aws_key_pair.keypair.key_name # Replace with your SSH key pair
+  key_name      = aws_key_pair.key-pair.key_name # Replace with your SSH key pair
   tags = {
     Name = "ALB instances"
     description = "EC2 instance "
@@ -20,7 +16,7 @@ resource "aws_launch_configuration" "ec2" {
   image_id            = var.os  # Replace with the desired AMI ID
   instance_type = var.instance     # Replace with the desired instance type
   security_groups             = [aws_security_group.ec2_sg.id]
-  key_name                    = aws_key_pair.keypair.key_name
+  key_name                    = aws_key_pair.key-pair.key_name
   associate_public_ip_address = false
   user_data = <<-EOL
   #!/bin/bash -xe
