60cef7bda861a205867fae4b6cb6d6d871d432e1
ebere.okey <okeyebereblessing@gmail.com>
2024-07-06 23:55:29 +0100

    Api-gateway error fixes

--- LAMBDA-API-GATEWAY/api-gateway.tf
+++ LAMBDA-API-GATEWAY/api-gateway.tf
@@ -1,21 +1,45 @@
-# Define API Gateway Resource
-resource "aws_api_gateway_rest_api" "api" {
-  name = "canary deployment"
-}
-# Define API Gateway Stages
-resource "aws_api_gateway_stage" "prod" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "stable"
-  deployment_id = aws_api_gateway_deployment.prod.id
+# API Gateway
+resource "aws_api_gateway_rest_api" "canary_api" {
+  name        = "canary_deployment"
+  description = "API for canary deployment"
 }
 
-  # Integration with Primary Lambda
-  resources = "{+}"
-  methods    = ["GET"]
-  type       = "AWS"
+resource "aws_api_gateway_resource" "canary_resource" {
+  rest_api_id = aws_api_gateway_rest_api.canary_api.id
+  parent_id   = aws_api_gateway_rest_api.canary_api.root_resource_id
+  path_part   = "canary"
+}
+resource "aws_api_gateway_method" "canary_method" {
+  rest_api_id   = aws_api_gateway_rest_api.canary_api.id
+  resource_id   = aws_api_gateway_resource.canary_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "canary_integration" {
+  rest_api_id = aws_api_gateway_rest_api.canary_api.id
+  resource_id = aws_api_gateway_resource.canary_resource.id
+  http_method = aws_api_gateway_method.canary_method.http_method
+  type        = "AWS_PROXY"
+  integration_http_method = "GET"
+  uri         = aws_lambda_function.canary_lambda.invoke_arn
+}
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.canary_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.canary_api.execution_arn}/*/*"
+}
+# Deployment
+resource "aws_api_gateway_deployment" "canary_deployment" {
+  depends_on = [
+    aws_api_gateway_method.canary_method,
+    aws_api_gateway_integration.canary_integration,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.todo_api.id
+  stage_name  = "prod"
+}
 
-  # Replace with your Lambda ARN
-  integration_settings = {
-    passthrough_header = ["Content-Type", "Authorization"]
-  }
-  uri = aws_lambda_function.canary_lambda.arn
+output "api_gateway_url" {
+  value = "${aws_api_gateway_deployment.todo_deployment.invoke_url}/todos"
+}
