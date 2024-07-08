21d44c9cb702b51d5fcef1f3fceff5665264e9be
ebere.okey <okeyebereblessing@gmail.com>
2024-07-08 12:34:24 +0100

    undo

--- LAMBDA-API-GATEWAY/main.tf
+++
@@ -1,9 +0,0 @@
-# Configure AWS Provider
-provider "aws" {
-  region = "us-east-1" # Replace with your desired region
- }
-
-module "current_lambda_module" {
-  source       = "./current_lambda_module"
-    }
-
