fa39010d9410001d9e80c96b99ab5ee0166780e0
Ebere Blessing <147432108+EbereBlessing@users.noreply.github.com>
2024-08-17 13:38:31 +0100

    Update api-gw.tf
    change myregion to region

--- API-GATEWAY-LAMBDA/api-gw.tf
+++ API-GATEWAY-LAMBDA/api-gw.tf
@@ -31,7 +31,7 @@ resource "aws_lambda_permission" "api_gateway" {
   action        = "lambda:InvokeFunction"
   function_name =  aws_lambda_function.canary_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.canary_api.id}/*/${aws_api_gateway_method.canary_method.http_method}${aws_api_gateway_resource.canary_resource.path}"
+  source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.canary_api.id}/*/${aws_api_gateway_method.canary_method.http_method}${aws_api_gateway_resource.canary_resource.path}"
 }
 # Deployment
 resource "aws_api_gateway_deployment" "canary_deployment" {
